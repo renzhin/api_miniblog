325a2acb7e2176ea454241f14dd94821ed750c1a
Михаил Ренжин <renzhin@gmail.com>
2023-10-24 08:39:14 +0300

    Зарегистрировать rest_framework + добавить базовый url.py + часть роутеров для приложения api

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+
+from api.views import UserViewSet, TitleViewSet
+
+app_name = 'api'
+
+v1_router = DefaultRouter()
+v1_router.register(r'users', UserViewSet, basename='users')
+v1_router.register(r'titles', TitleViewSet, basename='titles')
+
+urlpatterns = [
+    path('v1/', include(v1_router.urls)),
+]
