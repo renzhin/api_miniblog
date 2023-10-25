c0369ce03cb008a250cdce6ad0f884a650207cf0
Михаил Ренжин <renzhin@gmail.com>
2023-10-25 16:51:33 +0300

    Удалить комментарии в models.py приложения users

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,8 +1,5 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-# from .validators import UnicodeUsernameValidator
-
-# username_validator = UnicodeUsernameValidator()
 
 
 class MyUser(AbstractUser):
@@ -16,11 +13,7 @@ class MyUser(AbstractUser):
         (MODERATOR, 'Модератор'),
         (ADMIN, 'Администратор')
     ]
-    # username = models.CharField(
-    #     max_length=150,
-    #     unique=True,
-    #     validators=[username_validator],
-    # )
+
     email = models.EmailField(unique=True)
     password = models.CharField(
         'Пароль',
@@ -30,7 +23,4 @@ class MyUser(AbstractUser):
     )
     role = models.CharField(max_length=2, choices=ROLE_CHOICES, default=USER)
     bio = models.TextField(blank=True)
-    # first_name = models.CharField(max_length=150, blank=True)
-    # last_name = models.CharField(max_length=150, blank=True)
     confirmation_code = models.CharField(max_length=150, blank=True)
-
