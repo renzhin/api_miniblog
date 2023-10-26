3ffc99e470a6e7109430ef2c66dace72640211cf
Михаил Ренжин <renzhin@gmail.com>
2023-10-26 12:53:43 +0300

    Добавить сериализаторы для всех моделей кроме TitleGenre

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+import datetime as dt
 
-from titles.models import Comment, Review
+from titles.models import Category, Genre, Title, GenreTitle, Comment, Review
 
 
 CHOICES = (
@@ -24,12 +25,12 @@ class CommentSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = ('id', 'author', 'text', 'review_id', 'pub_date')
+        fields = '__all__'
         model = Comment
         read_only_fields = ('review_id', 'pub_date',)
 
 
-class CommentSerializer(serializers.ModelSerializer):
+class ReviewSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         slug_field='username',
         read_only=True
@@ -37,6 +38,50 @@ class CommentSerializer(serializers.ModelSerializer):
     score = serializers.ChoiceField(choices=CHOICES)
 
     class Meta:
-        fields = ('id', 'author', 'text', 'score', 'title_id', 'pub_date')
+        fields = '__all__'
         model = Review
         read_only_fields = ('title_id', 'pub_date',)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        exclude = ('id', )
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        exclude = ('id', )
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer(many=True)
+    category = serializers.SlugRelatedField(slug_field='name')
+    rating = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Title
+        fields = '__all__'
+
+    def get_rating(self, obj):
+        return 0
+
+    def validate_year(self, value):
+        year = dt.date.today().year
+        if not (1000 < value <= year):
+            raise serializers.ValidationError('Проверьте год создания!')
+        return value
+
+    def create(self, validated_data):
+        genres = validated_data.pop('genre')
+        title = Title.objects.create(**validated_data)
+
+        for genre in genres:
+            current_genre, status = Genre.objects.get_or_create(
+                **genre)
+            GenreTitle.objects.create(
+                genre_id=current_genre, title_id=title)
+        return title
