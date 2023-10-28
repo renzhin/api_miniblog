f174c49cd1a09cdeba9a6783233a438cc65eb649
Бачинин Александр <zasadaikachan@yandex.ru>
2023-10-28 15:27:31 +0300

    Добавить пагинацию

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -112,3 +112,10 @@ AUTH_USER_MODEL = 'users.MyUser'
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+
+}
