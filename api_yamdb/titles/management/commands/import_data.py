2080eb947ab1132e22e662e6d7d202e44d4313fe
Бачинин Александр <zasadaikachan@yandex.ru>
2023-10-29 21:02:27 +0300

    Добавить скрипт для массового импорта, дополнить management-команду

--- api_yamdb/titles/management/commands/import_data.py
+++ api_yamdb/titles/management/commands/import_data.py
@@ -1,7 +1,9 @@
 import csv
+from time import sleep
 from typing import Any
 from django.core.management.base import BaseCommand, CommandParser
 from titles.models import Title, Category, Genre, GenreTitle, Review, Comment
+from users.models import MyUser
 
 
 class Command(BaseCommand):
@@ -27,12 +29,16 @@ class Command(BaseCommand):
             'genre': Genre,
             'genre_title': GenreTitle,
             'review': Review,
-            'comment': Comment
+            'comment': Comment,
+            'user': MyUser,
         }
 
         with open(csv_file, 'r', encoding='utf-8') as file:
             reader = csv.DictReader(file)
             for row in reader:
                 models[model].objects.create(**row)
+                self.stdout.write(self.style.NOTICE('.'), ending='')
+                self.stdout.flush()
+                sleep(0.001)
 
-        self.stdout.write(self.style.SUCCESS('Данные успешно загружены'))
+        self.stdout.write(self.style.SUCCESS('\nДанные успешно загружены'))
