2154d09360c8cdcafc419324d5fc9d201393b2f3
Бачинин Александр <zasadaikachan@yandex.ru>
2023-10-29 21:04:40 +0300

    Добавить файл скрипта массового импорта

---
+++ api_yamdb/import.sh
@@ -0,0 +1,7 @@
+python manage.py import_data category static/data/category.csv
+python manage.py import_data genre static/data/genre.csv
+python manage.py import_data title static/data/titles.csv
+python manage.py import_data genre_title static/data/genre_title.csv
+python manage.py import_data user static/data/users.csv
+python manage.py import_data review static/data/review.csv
+python manage.py import_data comment static/data/comments.csv
